494acd2999e6b71a360d3de19c3f36e99f15bb72
unknown <francisco.rojas-m@labinfo.is.escuelaing.edu.co>
2022-09-14 08:43:25 -0500

    it gone

--- src/main/java/com/example/demo/repositories/UsuarioRespository.java
+++ src/main/java/com/example/demo/repositories/UsuarioRespository.java
@@ -1,11 +1,8 @@
 package com.example.demo.repositories;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
 import com.example.demo.models.UsuarioModel;
 
-@Repository
-public interface UsuarioRespository extends CrudRepository<UsuarioModel, Long> {
-    
+public interface UsuarioRespository {
+    public Object findAll();
+    public UsuarioModel save(UsuarioModel usuario);
 }
---
+++ src/main/java/com/example/demo/repositories/UsuarioRespositoryImp.java
@@ -0,0 +1,20 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.UsuarioModel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioRespositoryImp implements  UsuarioRespository{
+
+    public UsuarioRespositoryImp() {
+
+    }
+
+    public Object findAll() {
+        return null;
+    }
+
+    public UsuarioModel save(UsuarioModel usuario) {
+        return null;
+    }
+}
